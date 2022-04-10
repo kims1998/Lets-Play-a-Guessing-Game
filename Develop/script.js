5967765b2abde60abe6808df05da08865c7b8914
Seung Kyem Kim <seung.kim1998@gmail.com>
2022-04-09 19:19:29 -0700

    added comments so that another user reading the code can understand

--- Develop/script.js
+++ Develop/script.js
@@ -24,8 +24,7 @@ const SymbolsArr = charSet(33, 47).concat(
     charSet(91, 96)
   ).concat(
     charSet(123, 126)
-);//These are the Special symbols being used for the password string, multiple concat due to the special characters
-  //being separated from each other.
+);//These are the Special symbols being used for the password string, multiple concat due to the special characters being separated from each other.
 
 //This function randomly generates a number
 function randomIndexGenerator(arrayLength) {
@@ -33,7 +32,7 @@ function randomIndexGenerator(arrayLength) {
 }
 
 
-function charSet(start, end) {
+function charSet(start, end) {  //this function will search the character code numbers and push it into a new array. 
   let CharArray = [];
   for (let i = start; i <= end; i++){
     CharArray.push(String.fromCharCode(i))
@@ -41,11 +40,11 @@ function charSet(start, end) {
   return CharArray;
 }
 
-
+//When the user inputs are chosen, this function will create a new array.
 function generatePassword() {
-  let userInputLength = Number(prompt("Select a number from 08 - 128 character for your password length."));
+  let userInputLength = Number(prompt("Select a number from 08 - 128 character for your password length.")); //this portion ask the user choose from 8 to 128 characters.
 
-  if (userInputLength >= 8 && userInputLength <= 128) {
+  if (userInputLength >= 8 && userInputLength <= 128) { 
     let passwordString = "";
 
     let poolArray = CharPool();
@@ -56,9 +55,9 @@ function generatePassword() {
     console.log(poolArray[randomIndexGenerator(poolArray.length)]);
 
     for (let indexPassword = 0; indexPassword < userInputLength; indexPassword++) {
-      passwordString = passwordString + poolArray[randomIndexGenerator(poolArray.length)]
+      passwordString = passwordString + poolArray[randomIndexGenerator(poolArray.length)] //
     ;}
-    console.log(passwordString);
+    console.log(passwordString); //in the event that they choose a number  < 8 and > 128, this'll prompt them to try again because it didn't meet the requirements.
       return passwordString
       ;} else {
     return "You chose a number that was not between 8 and 128 characters. Please try again."
@@ -66,35 +65,35 @@ function generatePassword() {
   }
 }
 
-function CharPool() {
+function CharPool() { 
 
-  let CharArray = [];
+  let CharArray = [];//this portion of codes will run when the user selects whether they want...
   if (confirm("Would you like to incorporate Uppercase Letters into your password?")) {
-    CharArray = CharArray.concat(UppersArr)
+    CharArray = CharArray.concat(UppersArr) //...Uppercase lettering in the password.
       ;
   }
 
   if (confirm("Would you like to add LowerCase Letters as well?")) {
-    CharArray = CharArray.concat(LowersArr)
+    CharArray = CharArray.concat(LowersArr) //...Lowercase lettering in the password.
       ;
   }
 
   if (confirm("Would you like to add Numbers to your password?")) {
-    CharArray = CharArray.concat(NumbersArr)
+    CharArray = CharArray.concat(NumbersArr) //...Numbers in the password.
       ;
   }
 
   if (confirm("Would you like to add Special Characters to your password?")) {
-    CharArray = CharArray.concat(SymbolsArr)
+    CharArray = CharArray.concat(SymbolsArr) //...Special characters included in their password.
       ;
   }
 
   if (CharArray.length === 0) {
     alert("Please choose at least one of the following options.")
-    return CharPool();
+    return CharPool(); //This if statement is here in the event that the user chooses not to select yes to any of the options.
   }
 
-  console.log(CharArray)
+  console.log(CharArray) //This will return the randomized characters.
   return CharArray;
 }
 
